95c82725e0f2599846322ed78a03c46c8daa8664
Pritish S Adiga <padiga@buffalo.edu>
2020-11-26 15:53:10 -0500

    stochastic_matrix codeblock

--- distribution_sample.py
+++ distribution_sample.py
@@ -22,3 +22,14 @@ f.fit()
 # may take some time since by default, all distributions are tried
 # but you call manually provide a smaller set of distributions
 f.summary()
+
+N = 1000000
+for i in range(N):    
+    matrix = np.random.uniform(low=0., high=0.99, size=(4, 4))
+    matrix = matrix / matrix.sum(axis=1, keepdims=1)
+
+    new_matrix = matrix[:-2]
+    abs_states = np.array([[0, 0, 1, 0], [0, 0, 0, 1]])
+    trans_matrix = np.concatenate((new_matrix,abs_states))
+    TP_Matrix = np.matrix(trans_matrix)
+    print(TP_Matrix)
