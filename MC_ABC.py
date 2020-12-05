611c152ec5c043188b029e510dd05f8fcb5e0b7f
Pritish S Adiga <padiga@buffalo.edu>
2020-12-04 19:51:51 -0500

    Update MC_ABC.py

--- MC_ABC.py
+++ MC_ABC.py
@@ -26,7 +26,8 @@ f.summary()
 def TMatrix_generator():    
     matrix = np.random.uniform(low=0., high=0.99, size=(4, 4))
     matrix = matrix / matrix.sum(axis=1, keepdims=1)
-
+    # matrix = np.random.normal(size=(4, 4))
+    # matrix = matrix / matrix.sum(axis=1, keepdims=1)
     new_matrix = matrix[:-2]
     abs_states = np.array([[0, 0, 1, 0], [0, 0, 0, 1]])
     trans_matrix = np.concatenate((new_matrix,abs_states))
@@ -36,15 +37,38 @@ def TMatrix_generator():
 # import pdb
 def MC_calculator(transition_matrix,present_state, days):
 
-    present_df = pd.DataFrame([present_state], columns=list('HCRD'))
-
-    for _ in range(days):
+    # present_df = pd.DataFrame([present_state], columns=list('HCRD'))
+  
+    array = [[i] for i in present_state]
+    # print(transition_matrix)
+    
+    for j in range(days):
         new_state = present_state * transition_matrix
-        new_state_1 = np.squeeze(np.asarray(new_state))
-        new_df = pd.DataFrame([new_state_1], columns=list('HCRD'))
-        present_df = present_df.append(new_df,ignore_index=True)
+        new_state = np.squeeze(np.asarray(new_state))
+
+        array[0].append(new_state[0])
+        array[1].append(new_state[1])
+        array[2].append(new_state[2])
+        array[3].append(new_state[3])
+        
         present_state = new_state
-    return present_df
+        
+    # for i in range(days):
+    #     new_state = present_state * transition_matrix
+    #     new_state_1 = np.squeeze(np.asarray(new_state))
+    #     for i in new_state_1:
+    #          somelist.append([i])
+
+    #     # new_df = pd.DataFrame([new_state_1], columns=list('HCRD'))
+    #     # tmatrix_df = pd.DataFrame([t_matrix], columns=list('T_Matrix'))
+    #     # tmatrix_df = pd.DataFrame(data=transition_matrix.flatten(), columns=["Transition Matrix"])
+    #     # present_df = present_df.append(new_df,ignore_index=True)
+    #     # present_df = present_df.append(tmatrix_df,ignore_index=True)
+    
+    transition_matrix = np.squeeze(np.asarray(transition_matrix))
+    array.append(transition_matrix)
+
+    return array
   
 import matplotlib.pyplot as plt
 plt.plot(sim_1['H'],color='red',label='Hospitalized')
