2fded701f5b9bf71db937a12fe7d728a761897be
Pritish S Adiga <padiga@buffalo.edu>
2021-01-07 10:23:02 -0500

    Update MC_ABC.py

--- MC_ABC.py
+++ MC_ABC.py
@@ -34,69 +34,27 @@ f.summary()
 #     TP_Matrix = np.matrix(trans_matrix)
 #     return TP_Matrix
 
-def TP_Matrix_generator(row_low, row_high):
+# 1. Logarthmic Function for Increasing and Decreasing Trends
 
-    HH = np.random.uniform(low = row_low , high = row_high)
+def NHC(init_value,limit,rate):
 
-    CH_UL = 1 - HH
-    CH_LL = 0
-    CH = np.random.uniform(low = CH_LL , high = CH_UL)
+    import math
+    NHC_list = []
+    present_state =init_value
+    for i in range(limit):
 
-    RH_UL = 1 - (HH + CH)
-    RH_LL = 0
-    RH = np.random.uniform(low = RH_LL, high = RH_UL)
-
-    HCR_sum = HH + CH + RH
-    DH = 1 - HCR_sum
-
-    row = [HH, CH, RH, DH]
+        next_state = present_state * math.exp(rate)
+        NHC_list.append(next_state)
+        present_state = next_state
     
-    return row
+    return NHC_list
 
-# np.sum(row_1)
-row_1 = TP_Matrix_generator(0.7, 1)
-row_2 = TP_Matrix_generator(0.5,0.9)
-row_3 = np.array([0,0,1,0])
-row_4 = np.array([0,0,0,1])
+# 2. Transition Probability Matrix Generator (A)
 
-TP_Matrix = np.matrix([row_1,row_2,row_3,row_4])
+def TMatrix_generator(x,y):    
 
-# TP_Matrix[3].sum()
-
-def TP_Matrix_generator(row_low, row_high):
-
-    HH = np.random.uniform(low = row_low , high = row_high)
-
-    CH_UL = 1 - HH
-    CH_LL = 0
-    CH = np.random.uniform(low = CH_LL , high = CH_UL)
-
-    RH_UL = 1 - (HH + CH)
-    RH_LL = 0
-    RH = np.random.uniform(low = RH_LL, high = RH_UL)
-
-    HCR_sum = HH + CH + RH
-    DH = 1 - HCR_sum
-
-    row = [HH, CH, RH, DH]
-    
-    return row
-
-# np.sum(row_1)
-row_1 = TP_Matrix_generator(0.7, 1)
-row_2 = TP_Matrix_generator(0.5,0.9)
-row_3 = np.array([0,0,1,0])
-row_4 = np.array([0,0,0,1])
-
-TP_Matrix = np.matrix([row_1,row_2,row_3,row_4])
-
-# TP_Matrix[3].sum()
-
-
-# import pdb
-def TMatrix_generator():    
-
-    matrix = np.random.uniform(low=0., high=0.99, size=(4, 4))
+    # matrix = np.random.uniform(low=0., high=0.99, size=(4, 4))
+    matrix = np.random.uniform(low = x , high = y, size=(4,4))   
     matrix = matrix / matrix.sum(axis=1, keepdims=1)
     # matrix = np.random.normal(size=(4, 4))
     # matrix = matrix / matrix.sum(axis=1, keepdims=1)
@@ -105,22 +63,54 @@ def TMatrix_generator():
     trans_matrix = np.concatenate((new_matrix,abs_states))
     TP_Matrix = np.matrix(trans_matrix)
     return TP_Matrix
-    
-def simulationABC(initial_values, days, no_of_simulations):
 
+# 2. Transition Probability Matrix Generator (B)
+'''
+def TP(average_value):
+    x = average_value + factor * average_value 
+    y = average_value - factor * average_value
+    gen = np.random.uniform(low = x, high = y)
+    return gen
+'''
+
+def Trans_Matrix(x,y):
+    A = np.random.uniform(low = x, high = y)
+    B_UL = 1 - A
+    B_LL = 0
+    B = np.random.uniform(low = B_LL , high = B_UL)
+    C_UL = 1 - B
+    C_LL = 0
+    C = np.random.uniform(low = C_LL, high = C_UL)
+    total_sum = A + B + C
+    D = 1- total_sum
+
+    row_values = [A, B, C, D]
+
+    return row_values
+
+# 3. Simulation 
+
+def simulationABC(initial_values, days, no_of_simulations, TP_mat):
+    '''
+    args:
+        initial_values- The current state of hospitalized, critical care, recovered and death
+        days = No. of days for which the simulation has to be run
+        no_of_simulations - No of total times the the Transition Matrix has to be simulated    
+    '''
     main_array = []
+    
 
     for i in range(no_of_simulations):
 
+        current_state = initial_values
         sub_array = [[i] for i in initial_values]
-
+        TP_Matrix = TP_mat
+        
         for j in range(days):
-            
-            current_state = initial_values
-            TP_Matrix = TMatrix_generator()
+              
             new_state = current_state * TP_Matrix
             new_state = np.squeeze(np.asarray(new_state))
-
+            
             sub_array[0].append(new_state[0])
             sub_array[1].append(new_state[1])
             sub_array[2].append(new_state[2])
@@ -132,12 +122,26 @@ def simulationABC(initial_values, days, no_of_simulations):
         sub_array.append(TP_Matrix)
 
         main_array.append(sub_array)
-
-
+        
     return main_array
+
+
+def posterior(obs_data,sim_data):
   
-import matplotlib.pyplot as plt
-plt.plot(sim_1['H'],color='red',label='Hospitalized')
-plt.plot(sim_1['C'],color='green',label='Critical Care')
-plt.plot(sim_1['R'],color='yellow',label='Recovered')
-plt.plot(sim_1['D'],color='blue',label='Death')
+    
+    accept = []
+    for i in range(len(sim_data)):
+        
+        x = obs_data
+
+        y = sim_data[i][0]
+        y = obs_data
+        K_S_Test = ks_2samp(x,y)
+        p_value = K_S_Test.pvalue
+
+        if(p_value >= 0.05):
+            
+            accept.append(sim_data[i])
+    
+
+    return accept
